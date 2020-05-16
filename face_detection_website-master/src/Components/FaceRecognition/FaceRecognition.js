606df73d398b3a12e556e8a8caa0733f772facf8
sparsh0427 <sparsh.chadha27@gmail.com>
2020-05-16 18:01:59 +0530

    Added frontend files

---
+++ face_detection_website-master/src/Components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './FaceRecognition.css';
+
+
+const FaceRecognition = ({imageUrl, box}) => {
+    return(
+        <div className='center ma'>
+            <div className='mt2 absolute'>
+                <img id='inputimage' src={imageUrl} alt="image_of_url" width='500px' height='auto'/>
+                <div 
+                className='bounding-box' 
+                style={{top: box.topRow,right: box.rightCol,bottom: box.bottomRow,left:box.leftCol}}></div>
+            </div>
+        </div>
+    );
+}
+
+export default FaceRecognition;
